730aeaf97203f27e330d01e3e7973b026ad5cb4f
Tres Spicher <tspicher@ebay.com>
2021-10-13 15:53:41 -0700

    Initial Commit

---
+++ SignpostTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SignpostTest
+//
+//  Created by Spicher, Tres on 10/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SignpostTest/SignpostTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignpostTestApp.swift
+//  SignpostTest
+//
+//  Created by Spicher, Tres on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SignpostTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
