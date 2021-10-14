eafcf4d2a3089b62b478d05fb94d05064981a72d
Tres Spicher <tspicher@ebay.com>
2021-10-14 15:29:13 -0700

    Initial commit

--- SignpostTest/SignpostTestApp.swift
+++ SignpostTest/SignpostTestApp.swift
@@ -5,10 +5,21 @@
 //  Created by Spicher, Tres on 10/13/21.
 //
 
+import os.signpost
 import SwiftUI
+
+let signpostLog = OSLog(subsystem: "com.tspike.signpost", category: "signpost")
+let signpostName: StaticString = "SignpostTest"
 
 @main
 struct SignpostTestApp: App {
+	init() {
+		os_signpost(.begin, log: signpostLog, name: signpostName)
+		DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500), execute: {
+			os_signpost(.end, log: signpostLog, name: signpostName)
+		})
+	}
+	
     var body: some Scene {
         WindowGroup {
             ContentView()
